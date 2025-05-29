281faabfae1e1f3ccbe606af6d485f083a51296f
xodyd123 <gksrmf9186@naver.com>
2025-05-29 14:01:54 +0900

    feat : diary 상세 조회 구현

--- src/main/java/mypicday/store/diary/repository/DiaryRepository.java
+++ src/main/java/mypicday/store/diary/repository/DiaryRepository.java
@@ -15,6 +15,20 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
 
+    // TODO 친구 목록으로 인한 쿼리 및 전체 목록 나중에 하고 일단 공통된 반환 값만 나오게 설정
+    @Query("""
+    select d
+    from Diary d
+    where   d.user.id = :loginUserId
+        or d.status <> 'PRIVATE'
+    order by d.createdAt desc
+    """)
+    //or (d.user.id <> :loginUserId and d.status <> 'PRIVATE')
+    Page<Diary> findAllByAuthorId(
+            @Param("loginUserId") String loginUserId,
+            Pageable pageable
+    );
+
     @Query("SELECT d.like.count FROM Diary d WHERE d.id = :diaryId")
     long LikeCountByDiaryId(@Param("diaryId") Long diaryId);
 
