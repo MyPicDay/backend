e9a2effde60d19e0be27c343bdceef5ef5821f5f
gogogotoh <songhoyoung31@gmail.com>
2025-06-04 00:09:08 +0900

    Update UserResponseDTO.java

--- src/main/java/mypicday/store/follow/dto/UserResponseDTO.java
+++ src/main/java/mypicday/store/follow/dto/UserResponseDTO.java
@@ -17,23 +17,4 @@ public class UserResponseDTO {
     private long followerCount;
     private long followingCount;
     private boolean following;
-    
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import mypicday.store.user.entity.User;
-
-@Data
-@AllArgsConstructor
-public class UserResponseDTO {
-    private String id;
-    private String name;
-    private String avatar;
-    private long followersCount;
-    private long followingsCount;
-
-    public UserResponseDTO(User user) {
-        this.id = user.getId();
-        this.name = user.getNickname();
-        this.avatar = user.getAvatar();
-    }
 }
